bc028af24feac73238b33c06c0516fc69c6ebe76
ILhankhondaker <ilhankhondaker@gmail.com>
2025-09-25 14:14:41 +0600

    add navbar active route

--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -21,7 +21,7 @@ export function Navbar() {
   ]
 
   return (
-    <nav className="bg-gray-900 border-b border-gray-800 py-3 sticky top-0 z-50">
+    <nav className="bg-[#131313] border-b  border-gray-800 py-3 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
