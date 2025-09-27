b91526b0304ff935a8c578e6e2769ae3fd2c7a0a
ILhankhondaker <ilhankhondaker@gmail.com>
2025-09-27 16:06:28 +0600

    add talent card

--- src/app/creators/page.tsx
+++ src/app/creators/page.tsx
@@ -1,4 +1,5 @@
 import DownloadAgent from '@/components/agent/DownloadAgent'
+import { TalentGrid } from '@/components/creators/TalentGrid'
 import PageTransition from '@/components/PageTransition'
 import HeroSection from '@/components/Shared/Hero'
 import React from 'react'
@@ -9,6 +10,13 @@ const Page = () => {
 
     <div>
           <HeroSection image={'/creators.png'} title={'Discover the Brilliant Faces'} brTitle={' Driving Innovation.'} description={'Meet the creators shaping trends, building influence, and inspiring collaborations worldwide.'} />
+
+  <main className="min-h-screen bg-[#131313] text-white">
+      <div className="container mx-auto px-4 py-8">
+        <TalentGrid />
+      </div>
+    </main>
+
 
                 <DownloadAgent />
     </div>
