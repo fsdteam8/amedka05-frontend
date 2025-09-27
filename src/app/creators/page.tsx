4205d11c893f6d73f868776476741dcbb0b39dfa
ILhankhondaker <ilhankhondaker@gmail.com>
2025-09-27 14:29:06 +0600

    add creators hero

--- src/app/creators/page.tsx
+++ src/app/creators/page.tsx
@@ -1,12 +1,15 @@
+import PageTransition from '@/components/PageTransition'
+import HeroSection from '@/components/Shared/Hero'
 import React from 'react'
 
 const Page = () => {
   return (
+   <PageTransition>
+
     <div>
-
-
-        <h1>creator page   </h1>
+          <HeroSection image={'/agentImage.png'} title={'Discover the Brilliant Faces'} brTitle={' Driving Innovation.'} description={'Meet the creators shaping trends, building influence, and inspiring collaborations worldwide.'} />
     </div>
+   </PageTransition>
   )
 }
 
