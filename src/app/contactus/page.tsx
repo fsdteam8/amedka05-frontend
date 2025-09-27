68c2977f768b93bb1d2b4d1d284602a0eddc9844
mehedishishir0 <mehedihasanshishir@gmail.com>
2025-09-27 12:55:13 +0600

    fixup

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -1,3 +1,4 @@
+import PageTransition from '@/components/PageTransition'
 import ContactInfo from '@/components/Shared/ContactInfo'
 import ContactUsForm from '@/components/Shared/ContactUs'
 import HeroSection from '@/components/Shared/Hero'
@@ -6,11 +7,14 @@ import React from 'react'
 
 const page = () => {
     return (
+        <PageTransition>
+
         <div>
             <HeroSection image={'/contactuspic1.png'} title={'Your Next Big Collaboration'} brTitle={'Begins Here.'} description={'our journey with creators, brands, and opportunities starts with a simple message.'} />
             <ContactUsForm />
             <ContactInfo />
         </div>
+        </PageTransition>
     )
 }
 
