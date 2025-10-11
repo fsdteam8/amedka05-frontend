af95e6f56f09599bae26fa3f5f13621af06b7d81
ILhankhondaker <ilhankhondaker@gmail.com>
2025-10-11 12:38:28 +0600

    build back button to cathc windows history

--- src/app/agent-form/page.tsx
+++ src/app/agent-form/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import AgentForm from './_components/agentFormBody'
 import { Button } from '@/components/ui/button'
 import { ArrowLeft } from 'lucide-react'
+import BackButton from '../../components/BackButton'
 
 const page = () => {
   return (
@@ -17,16 +18,12 @@ const page = () => {
     >
 
       <div className="max-w-7xl mx-auto">
-        <Button
-          variant="ghost"
-          size="icon"
-          className="mb-8 text-white hover:bg-white/10"
-        >
-          <ArrowLeft className="h-5 w-5" />
-        </Button>
+     
+
+        <BackButton/>
 
         <h1 className="text-[#7DD3DD] font-normal text-center text-[40px] mb-10 ">
-          Collaborate For Growth
+          Collaborate For Growth 
         </h1>
            <AgentForm/>
       </div>
